310e93f0f3fc80e97fd63015854c586a07b30c2f
mac <m1901321@edu.misis.ru>
2022-10-20 00:31:50 +0300

    Initial commit

---
+++ src/main/java/com/example/springsecurity/model/Developer.java
@@ -0,0 +1,14 @@
+package com.example.springsecurity.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor @NoArgsConstructor
+@Data
+public class Developer {
+    private Long id;
+    private String firstName;
+    private String lastNAme;
+}
